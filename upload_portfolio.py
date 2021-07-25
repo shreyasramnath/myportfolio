0c2bf5e45fcb583eb88d589666105d0176a6941b
shreyasramnath <shreyas.ramnath@idexcel.com>
2021-07-25 19:44:30 -0400

    added sns code

--- upload_portfolio.py
+++ upload_portfolio.py
@@ -3,7 +3,7 @@ import boto3
 import io
 import zipfile
 def lambda_handler(event, context):
-
+    sns = boto3.resource('sns')
     s3 = boto3.resource('s3')
     portfolio_bucket = s3.Bucket('portfolio.shreyasramnath.com')
     build_bucket = s3.Bucket('portfoliobuild.shreyasramnath.com')
@@ -15,7 +15,9 @@ def lambda_handler(event, context):
             obj = myzip.open(nm)
             portfolio_bucket.upload_fileobj(obj, nm)
             portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-
+    topic = sns.Topic('arn:aws:sns:us-east-1:048847678797:portfolioTopic')
+    topic.publish(Subject='Test', Message='')
+    topic.publish(Subject='Test', Message='k')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
