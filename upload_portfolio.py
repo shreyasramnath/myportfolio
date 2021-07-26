8ddd6ea9a4f409e063bf652c987087e7f3bc413a
shreyasramnath <shreyas.ramnath@idexcel.com>
2021-07-25 20:16:17 -0400

    change for codepipeline

--- upload_portfolio.py
+++ upload_portfolio.py
@@ -5,21 +5,34 @@ import zipfile
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
     s3 = boto3.resource('s3')
+    location = {
+        "bucketName": "portfoliobuild.shreyasramnath.com",
+        "objectKey": "portfoliobuild.zip"
+    }
     try:
+        job = event.get('CodePipeline.job');
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "portfolio":
+                    location = artifact["location"]["s3Location"]
+                
         portfolio_bucket = s3.Bucket('portfolio.shreyasramnath.com')
-        build_bucket = s3.Bucket('portfoliobuild.shreyasramnath.com')
+        build_bucket = s3.Bucket(location["bucketName"])
         build_bucket.download_file('portfoliobuild.zip', '/tmp/portfoliobuild.zip')
         portfolio_zip = io.BytesIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
                 portfolio_bucket.upload_fileobj(obj, nm)
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
         topic = sns.Topic('arn:aws:sns:us-east-1:048847678797:portfolioTopic')
-        topic.publish(Subject='Test', Message='k')
+        topic.publish(Subject='Success', Message='successful')
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except: 
-            topic.publish(Subject='fail', Message='f')
+            topic.publish(Subject='Fail', Message='failure')
             raise
     return {
         'statusCode': 200,
