e49c0502aed111b6e719b40c54fec022ae124973
shreyasramnath <shreyas.ramnath@idexcel.com>
2021-07-25 22:24:37 -0400

    changed python script

--- upload_portfolio.py
+++ upload_portfolio.py
@@ -19,7 +19,6 @@ def lambda_handler(event, context):
                 
         portfolio_bucket = s3.Bucket('portfolio.shreyasramnath.com')
         build_bucket = s3.Bucket(location["bucketName"])
-        build_bucket.download_file('portfoliobuild.zip', '/tmp/portfoliobuild.zip')
         portfolio_zip = io.BytesIO()
         build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
         with zipfile.ZipFile(portfolio_zip) as myzip:
